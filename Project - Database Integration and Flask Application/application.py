894805ce734fecb92203c4122b295077d090db4a
shuaibr <31935226+shuaibr@users.noreply.github.com>
2020-06-01 15:07:34 -0400

    connected db and set flask pages

---
+++ Project - Database Integration and Flask Application/application.py
@@ -0,0 +1,40 @@
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import create_engine
+from flask_session import Session
+from flask import Flask, session, render_template
+import os
+import requests
+res = requests.get("https://www.goodreads.com/book/review_counts.json",
+                   params={"key": "BgzQ9doaTztk9S8YBTVefg", "isbns": "9781632168146"})
+print(res.json())
+
+
+app = Flask(__name__)
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/thoughts")
+def thoughts():
+    return render_template("thoughts.html")
+
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Harvard-CS50-Web-Programming
+
+This project repo covers:
+• Git
+• HTML, CSS
+• Flask
+• SQL
+• ORMs, APIs
+• JavaScript
+• Front Ends
+• Django
+• Testing, CI/CD
+• GitHub, Travis CI
+• Scalability
+• Security
